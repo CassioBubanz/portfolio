6bc81961f2a8b4730bd130a01c7eb950816dbdc0
CassioBubanz <fcassiano2013@gmail.com>
2024-09-30 22:40:09 -0300

    feat: modelo pagina inicial

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/cassio_bubanz/" className="home__social-icon" target='_blank'>
+        <i className="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://github.com/CassioBubanz" className="home__social-icon" target='_blank'>
+        <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/francisco-cassiano/" className="home__social-icon" target='_blank'>
+        <i class="uil uil-linkedin-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
